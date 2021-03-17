164b1c177fbe3160eeb5d7f8a02b0d68b602813c
machigatta <iappware@googlemail.com>
2021-03-17 15:06:22 +0100

    BookNavigation proto / restored Level 1 / Resources added

--- Assets/Scripts/BookHandler.cs
+++ Assets/Scripts/BookHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BookHandler : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class BookHandler : MonoBehaviour
     private GameObject buttonNext;
     private GameObject buttonPrevious;
     public bool isReading;
+    private int clueIndex = 0;
 
     public void Awake()
     {
@@ -25,13 +27,41 @@ public class BookHandler : MonoBehaviour
     {
         if (Input.GetKeyDown("q"))
         {
-            BookPage.Play();
+            if (clueIndex > 0)
+            {
+                BookPage.Play();
+                clueIndex--;
+            }
         }
         else if (Input.GetKeyDown("e"))
         {
             BookPage.Play();
+            clueIndex++;
         }
+
+        loadPageContent();
         patchVisibility();
+    }
+
+    private void loadPageContent()
+    {
+        //todo grep all data and show based on index
+        if (clueIndex > 0)
+        {
+            GameObject.Find("Clue-Book/Headline").GetComponent<Text>().text = "Heracles fight with the Nemean lion";
+            GameObject.Find("Clue-Book/Author").GetComponent<Text>().text = "by Pieter Paul Rubens";
+            GameObject.Find("Clue-Book/Description").GetComponent<Text>().text = "Heracles wandered the area until he came to the town of Cleonae. There he met a boy who said that if Heracles slew the Nemean lion and returned alive within 30 days, the town would sacrifice a lion to Zeus; but if he did not return within 30 days or he died, the boy would sacrifice himself to Zeus. Another version claims that he met Molorchos, a shepherd who had lost his son to the lion, saying that if he came back within 30 days, a ram would be sacrificed to Zeus. If he did not return within 30 days, it would be sacrificed to the dead Heracles as a mourning offering.";
+            GameObject.Find("Clue-Book/Image").GetComponent<Image>().sprite = Resources.Load<Sprite>("Images/Heracles_fight_with_the_nemean_lion");
+        }
+        else
+        {
+            GameObject.Find("Clue-Book/Headline").GetComponent<Text>().text = "Bust of Emperor Commodus";
+            GameObject.Find("Clue-Book/Author").GetComponent<Text>().text = "";
+            GameObject.Find("Clue-Book/Description").GetComponent<Text>().text = "Here, the Roman Emperor has taken on the guise of the mythological hero, __Heracles__. He has been given the attributes of the hero: the lion skin placed over his head, the club placed in his right hand, and the golden apples of Hesperides in his left. Each of these objects has been placed as a reminder of the hero's accomplishments, as well as allowing the Emperor to associate and refer to himself as the Roman Hercales.";
+            GameObject.Find("Clue-Book/Image").GetComponent<Image>().sprite = Resources.Load<Sprite>("Images/Buste_Of_Emperor_Commodus");
+        }
+        
+
     }
 
     private void patchVisibility()
