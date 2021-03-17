c3a173c8b2e73ac9aee7d1ca8bf9316ed1f3d46c
CrystallDEV <m.hasselder@gmx.de>
2021-03-17 19:05:27 +0100

    Improve book behaviour

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,15 +7,66 @@ namespace Player
     {
         private Camera mainCamera;
 
+        public GameObject codexBook;
+        private BookHandler bookComponent;
+
         private void Awake()
         {
             mainCamera = Camera.main;
+            bookComponent = codexBook.GetComponent<BookHandler>();
         }
 
         private void Update()
         {
+            CheckInteraction();
+            CheckBookInput();
+            MoveMainCamera();
+        }
+
+        private void MoveMainCamera()
+        {
             var position = transform.position;
             mainCamera.transform.position = new Vector3(position.x, position.y, position.z - 10);
+        }
+
+        private void CheckInteraction()
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                // 5 is the maximum interaction range
+                Collider2D[] results = new Collider2D[5];
+                Physics2D.OverlapCircleNonAlloc(transform.position, 5, results);
+                foreach (var result in results)
+                {
+                    if (result == null)
+                    {
+                        continue;
+                    }
+
+                    InteractableObject interactableObject = result.gameObject.GetComponent<InteractableObject>();
+                    if (interactableObject == null || Vector2.Distance(transform.position, result.transform.position) <=
+                        interactableObject.interactionRange)
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+
+        private void CheckBookInput()
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                bookComponent.toggleVisibility();
+            }
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                bookComponent.IncreasePage();
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                bookComponent.DecreasePage();
+            }
         }
     }
 }
